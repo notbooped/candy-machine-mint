71a359e4b71fe5462fb88785ef371dc5ae6adfa2
notbooped <notbooped@gmail.com>
2021-12-15 13:31:51 +1000

    Initial Commit

--- src/Home.tsx
+++ src/Home.tsx
@@ -167,13 +167,15 @@ const Home = (props: HomeProps) => {
 
   return (
     <main>
+      <div className='mint-container'>
+
       {wallet && (
         <p>Wallet {shortenAddress(wallet.publicKey.toBase58() || "")}</p>
       )}
 
       {wallet && <p>Balance: {(balance || 0).toLocaleString()} SOL</p>}
 
-      {wallet && <p>Total Available: {itemsAvailable}</p>}
+      {wallet && <p>Total Supply: {itemsAvailable}</p>}
 
       {wallet && <p>Redeemed: {itemsRedeemed}</p>}
 
@@ -220,6 +222,7 @@ const Home = (props: HomeProps) => {
           {alertState.message}
         </Alert>
       </Snackbar>
+      </div>
     </main>
   );
 };
